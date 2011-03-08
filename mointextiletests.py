cbec249594ac30dec79e6343482543619e113798
Andrei Mackenzie <andrei@ccs.neu.edu>
2011-03-08 08:56:55 -0500

    Completed testing of the default 'http'/'mailto' anchor class attributes.
    This plugin now adds the 'http' class to all links that start with "http" and for which no other class is specified. This is done to emulate Moin's behavior of preceding external links with a little globe icon. The same idea is implemented for 'mailto.'

---
+++ mointextiletests.py
@@ -0,0 +1,32 @@
+"""
+    Tests for the mointextile plugin.
+
+    @copyright: 2011 by Andrei Mackenzie
+    @license: GNU GPL
+"""
+
+import unittest
+import mointextile
+
+class TextileTester(unittest.TestCase):
+    """Tests for mointextile.Textiler"""
+    def test_http(self):
+        """Test the default 'http' class"""
+        result = mointextile.Parser.get_processed('"Andrei Mackenzie":http://www.andreimackenzie.com')
+        self.assertEqual('\t<p><a href="http://www.andreimackenzie.com" class="http">Andrei Mackenzie</a></p>', result)
+
+    def test_existing_class(self):
+        """Test that existing classes are not overwritten"""
+        result = mointextile.Parser.get_processed('"(testA,testB)title":http://www.google.com')
+        self.assertEqual('\t<p><a href="http://www.google.com" class="testA,testB">title</a></p>', result)
+
+    def test_mailto(self):
+        """Test mailto links"""
+        result = mointextile.Parser.get_processed('"address":mailto:someone@somedomain.com')
+        self.assertEqual('\t<p><a href="mailto:someone@somedomain.com" class="mailto">address</a></p>', result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
